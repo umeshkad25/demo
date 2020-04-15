8e870dc9dd8fb0281d115dc0f14a8528959b71e4
umeshkad25 <umeshkad2025@gmail.com>
2020-04-15 16:16:14 +0530

    first commit

---
+++ src/third/FOR_LOOP.java
@@ -0,0 +1,14 @@
+package FOR_LOOP;
+
+public class FOR_LOOP 
+{
+ public static void main(String[] args)
+ {
+	 int c;
+	 for(c=0;c<10;c++)
+	 {
+		 System.out.println(""+c);
+	 }
+ 
+ }
+}
---
+++ src/third/third.java
@@ -0,0 +1,19 @@
+package third;
+
+public class third {
+
+	public static void main(String[] args) 
+	{
+		int c;
+		 for(c=0;c<10;c++)
+		 {
+			 System.out.println(""+c);
+		 }
+	 
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+	 
